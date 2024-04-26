7858c3a89e482a4a45c902cdc6eea410fef8c469
akifer <guipavanis2@gmail.com>
2024-04-26 01:27:08 -0300

    fixed typos

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
-const env = require('dotenv')
+const env = require("dotenv");
 
 const app = express();
 const port = 3201;
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+env.config()
 
 app.post('/api/sendEmail', (req, res) => {
     const { email, title, message } = req.body;
@@ -21,14 +22,14 @@ app.post('/api/sendEmail', (req, res) => {
     const transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
-            user: env.process(GMAIL),
-            pass: env.process(GMAIL_KEY)
+            user: process.env.GMAIL,
+            pass: process.env.GMAIL_KEY,
         },
     });
 
     const mailOptions = {
         from: email,
-        to: '#',
+        to: process.env.GMAIL,
         subject: `Nova mensagem: ${title}`,
         text: `Mensagem de: ${email}\n\n${message}`,
     };
